97e1b7a5bbdd13a992fe18a0b51feb7499786719
gana3011 <gana301104@gmail.com>
2025-09-06 16:07:22 +0530

    generate url for viewing files

--- src/controller/mediaController.js
+++ src/controller/mediaController.js
@@ -1,5 +1,11 @@
 import { pool } from "../../utils/database.js";
 
+function sanitizeFileName(name) {
+  return name
+    .replace(/[^a-zA-Z0-9.\-_]/g, "_"); 
+    // keep only letters, numbers, dot, dash, underscore
+}
+
 export const media = async(req, res) => {
     const supabase = req.app.locals.supabase;
     
@@ -9,7 +15,10 @@ export const media = async(req, res) => {
 
         if(!file) return res.status(400).send({message: 'File is required'});
 
-        const {data, error} = await supabase.storage.from("FileService").upload(file.originalname, file.buffer, {
+        const safeName = sanitizeFileName(file.originalname);
+        const filePath = `${Date.now()}-${safeName}`;
+
+        const {data, error} = await supabase.storage.from("FileService").upload(filePath, file.buffer, {
             contentType: file.mimetype,
             upsert: false,
         });
@@ -18,10 +27,35 @@ export const media = async(req, res) => {
             return res.status(500).send({message: error.message});
         }
 
-        const result = await pool.query(`insert into media_assets(title, type, file_url) values ($1,$2,$3)`, [title, type, data.path]);
-        return res.status(200).send({message: 'File uploaded successfully'});
+        const result = await pool.query(`insert into media_assets(title, type, file_url) values ($1,$2,$3) returning *`, [title, type, data.path]);
+        return res.status(200).send(result.rows[0]);
     } catch (error) {
         console.error(error);
         res.status(500).send({message: error.message});
+    }
+}
+
+
+export const streamUrl = async(req, res) => {
+    const supabase = req.app.locals.supabase;
+
+    try{
+        
+    const {id} = req.params;
+    const media = await pool.query(`select * from media_assets where id=$1`,[id]);
+    if(media.rowCount==0) return res.status(404).send({message: 'No media found'});
+
+    const filePath = media.rows[0].file_url;
+
+    await pool.query(`insert into media_view_logs(media_id, viewed_by_ip) values($1,$2)`,[id, req.ip]);
+
+    const {data, error} = await supabase.storage.from("FileService").createSignedUrl(filePath, 60*10);
+
+    if(error) return res.status(500).send({message: error.message});
+
+    return res.status(200).send({url: data.signedUrl});
+    } catch(error){
+        console.error(error);
+        return res.status(500).send({message: error.message});
     }
 }
--- src/routes/mediaRoutes.js
+++ src/routes/mediaRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { CheckAuth } from "../middleware/check-auth.js";
 import multer from "multer";
-import { media } from "../controller/mediaController.js";
+import { media, streamUrl } from "../controller/mediaController.js";
 
 const router = express.Router();
 
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/", CheckAuth, upload.single("file"), media);
+
+router.get("/:id/stream-url", CheckAuth, streamUrl)
 
 export default router;
